35356ca3fb3976e6aa3a51b70d2d1d3c6a40bf8b
kingsotn <kingstonyip@gmail.com>
2024-06-10 16:59:52 -0700

    working on receipt, added some typing animations

--- src/components/PlaceOrderForm.tsx
+++ src/components/PlaceOrderForm.tsx
@@ -31,13 +31,13 @@ export const CustomRadio = (props: any) => {
 
 
 interface DateTimeSliderProps {
-    startDate: Date;
+    // startDate: Date;
     setStartDate: React.Dispatch<React.SetStateAction<Date>>;
     duration: number;
     setDuration: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const DateTimeSlider: React.FC<DateTimeSliderProps> = ({ startDate, setStartDate, duration, setDuration }) => {
+const DateTimeSlider: React.FC<DateTimeSliderProps> = ({ setStartDate, duration, setDuration }) => {
     const handleChange = (val: any) => {
         setDuration(val);
     };
@@ -62,40 +62,6 @@ const DateTimeSlider: React.FC<DateTimeSliderProps> = ({ startDate, setStartDate
         }
     };
 
-    const calculateEndDate = (start: Date, duration: number): Date => {
-        const startDateCopy = new Date(start);
-        if (duration <= 50) {
-            const hours = 1 + Math.floor((duration * 22) / 50);
-            startDateCopy.setHours(startDateCopy.getHours() + hours);
-        } else {
-            const days = 1 + Math.floor(((duration - 50) * 30) / 50);
-            startDateCopy.setDate(startDateCopy.getDate() + days);
-        }
-        return startDateCopy;
-    };
-
-    const formatDateTime = (date: Date): string => {
-        // Extract the necessary parts from the Date object
-        const options: Intl.DateTimeFormatOptions = {
-            month: 'short',
-            day: 'numeric',
-            year: 'numeric',
-            hour: 'numeric',
-            hour12: true,
-        };
-
-        // Extract month, day, year, and time with period using Intl.DateTimeFormat
-        const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
-        const [monthDay, year, timePeriod] = formattedDate.split(', ');
-        const [time, period] = timePeriod.split(" ");
-        // console.log(monthDay, year)
-        const formattedString = `${monthDay}, ${year} @${time}${period}`;
-
-        // console.log("formatted:", formattedString);
-        return formattedString;
-    };
-
-    const endDate = calculateEndDate(startDate, duration);
 
     // only for days before "today"
     const isDateUnavailable = (date: DateValue): boolean => {
@@ -118,8 +84,9 @@ const DateTimeSlider: React.FC<DateTimeSliderProps> = ({ startDate, setStartDate
         setStartDate(convertedDate);
         console.log("Selected date:", convertedDate);
     };
+
     return (
-        <div className="max-w-xl w-full space-y-4">
+        <div className="font-mono max-w-xl w-full space-y-4">
             <div className="flex flex-row gap-4">
                 <DatePicker
                     label="Start Date"
@@ -133,7 +100,7 @@ const DateTimeSlider: React.FC<DateTimeSliderProps> = ({ startDate, setStartDate
                     onChange={handleDateChange}
                 />
             </div>
-            <div className="flex items-center space-x-4 max-w-xl w-full border border-gray-300 rounded-lg p-2">
+            <div className="flex items-center space-x-4 max-w-xl w-full border border-gray-300 rounded-lg p-1">
                 <div className="flex-grow max-w-xs w-full">
                     <Slider
                         step={1}
@@ -141,6 +108,7 @@ const DateTimeSlider: React.FC<DateTimeSliderProps> = ({ startDate, setStartDate
                         color={'foreground'}
                         size="sm"
                         onChange={handleChange}
+                        aria-label="Duration slider"
                         classNames={{
                             base: "w-full",
                             track: "bg-gray-300 h-0.5 rounded-full",
@@ -156,30 +124,26 @@ const DateTimeSlider: React.FC<DateTimeSliderProps> = ({ startDate, setStartDate
                     <span>{formatValue(duration)} {getLabel(duration)}</span>
                 </div>
             </div>
-            <div className='font-mono text-xs ml-1.5'>
-                {formatDateTime(startDate)}
-            </div>
-            <div className='font-mono text-xs ml-1.5 mt-0 w-full align--baseline'>
-                {formatDateTime(endDate)}
-            </div>
         </div>
     );
 };
 
 
 
-const CustomForm: React.FC = () => {
-    const [price, setPrice] = useState<string>('');
-    const [gpus, setGpus] = useState<string>('');
-    const marketPrice = "4.31" // let's say it's this for now
-    const defaultGpuCount = "1"
-    const [isFilled, setIsFilled] = useState(false); // coloring the dollar sign
-
-    // date stuff
-    const initialDate = new Date();
-    initialDate.setHours(initialDate.getHours() + 1, 0, 0, 0); // Round to the next hour
-    const [startDate, setStartDate] = useState<Date>(initialDate);
-    const [duration, setDuration] = useState(25); // Default value
+const CustomForm: React.FC<{
+    price: string,
+    setPrice: React.Dispatch<React.SetStateAction<string>>,
+    gpus: string,
+    setGpus: React.Dispatch<React.SetStateAction<string>>,
+    // startDate: Date,
+    setStartDate: React.Dispatch<React.SetStateAction<Date>>,
+    duration: number,
+    setDuration: React.Dispatch<React.SetStateAction<number>>,
+    marketPrice: string,
+    defaultGpuCount: string,
+    setIsFilled: React.Dispatch<React.SetStateAction<boolean>>
+    isFilled: boolean;
+}> = ({ price, setPrice, gpus, setGpus, setStartDate, duration, setDuration, marketPrice, defaultGpuCount, setIsFilled, isFilled }) => {
 
     const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value;
@@ -231,10 +195,11 @@ const CustomForm: React.FC = () => {
             e.preventDefault();
 
             // Move focus to the next input if needed
-            const nextInput = document.getElementById(e.currentTarget.id === 'price-input' ? 'gpus-input' : 'price-input');
+            const nextInput = document.getElementById(nextInputId);
             nextInput?.focus();
             setIsFilled(true);
         } else if ((e.key === 'Enter' || e.key === 'Tab') && e.currentTarget.value !== "") {
+            console.log('here')
             if (e.currentTarget.id === 'price-input') {
                 const roundedPrice = Math.round(parseFloat(e.currentTarget.value) * 100) / 100;
                 setPrice(roundedPrice.toFixed(2)); // Ensure it's always two decimal places
@@ -245,16 +210,19 @@ const CustomForm: React.FC = () => {
             e.preventDefault();
 
             // Move focus to the next input if needed
-            const nextInput = document.getElementById(e.currentTarget.id === 'price-input' ? 'gpus-input' : 'price-input');
+            const nextInput = document.getElementById(nextInputId);
+            console.log('next', nextInput)
             nextInput?.focus();
+            setIsFilled(true);
         }
 
 
         if (key === 'Tab') {
+            console.log('here too')
             e.preventDefault();
             if (!shiftKey) {
-                setPlaceholderIfEmpty();
                 const nextInput = document.getElementById(nextInputId);
+                console.log('ni2', nextInput)
                 nextInput?.focus();
             } else if (shiftKey) {
                 const previousInput = document.getElementById(previousInputId);
@@ -280,7 +248,7 @@ const CustomForm: React.FC = () => {
                 }
                 endContent={<span className="text-default-400 text-xs">/gpuhr</span>}
                 onChange={handlePriceChange}
-                onKeyDown={(e) => handleKeyPress(e, 'gpus-input', 'previous-input-id', 'default-price')}
+                onKeyDown={(e) => handleKeyPress(e, 'gpus-input', 'price-input', marketPrice)}
                 errorMessage={"Enter a value"}
             />
             <Spacer y={1.5} />
@@ -293,30 +261,199 @@ const CustomForm: React.FC = () => {
                 id="gpus-input"
                 endContent={<span className="text-default-400 text-xs">amount</span>}
                 onChange={handleGpusChange}
-                onKeyDown={(e) => handleKeyPress(e, 'next-input-id', 'price-input', 'default-gpus')}
+                onKeyDown={(e) => handleKeyPress(e, 'date-time-slider', 'price-input', 'default-gpus')}
             />
-            <DateTimeSlider startDate={startDate} setStartDate={setStartDate} duration={duration} setDuration={setDuration} />
+            <Spacer y={1.5} />
+            <div id="date-time-slider">
+                <DateTimeSlider setStartDate={setStartDate} duration={duration} setDuration={setDuration} />
+            </div>
         </>
-
     );
 };
 
 
+interface ReceiptType {
+    refNumber: string;
+    orderType: string;
+    price: string;
+    gpuCount: string;
+    time: string;
+    total: number;
+    startDate: Date;
+    duration: number;
+}
+
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { AnimatePresence, motion } from "framer-motion"
+const Receipt: React.FC<{ receiptData: ReceiptType }> = ({ receiptData }) => {
+
+    const {
+        refNumber,
+        orderType,
+        price,
+        gpuCount,
+        time,
+        total,
+        startDate,
+        duration,
+    } = receiptData;
+
+    const formatDateTime = (date: Date): string => {
+        // Extract the necessary parts from the Date object
+        const options: Intl.DateTimeFormatOptions = {
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric',
+            hour: 'numeric',
+            hour12: true,
+        };
+
+        // Extract month, day, year, and time with period using Intl.DateTimeFormat
+        const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
+        const [monthDay, year, timePeriod] = formattedDate.split(', ');
+        const [time, period] = timePeriod.split(" ");
+        // console.log(monthDay, year)
+        const formattedString = `${monthDay}, ${year} @${time}${period}`;
+
+        // console.log("formatted:", formattedString);
+        return formattedString;
+    };
+
+    const calculateEndDate = (start: Date, duration: number): Date => {
+        const startDateCopy = new Date(start);
+        if (duration <= 50) {
+            const hours = 1 + Math.floor((duration * 22) / 50);
+            startDateCopy.setHours(startDateCopy.getHours() + hours);
+        } else {
+            const days = 1 + Math.floor(((duration - 50) * 30) / 50);
+            startDateCopy.setDate(startDateCopy.getDate() + days);
+        }
+        return startDateCopy;
+    };
+
+    const endDate = calculateEndDate(startDate, duration);
+
+    console.log("ref", refNumber)
+
+    const receiptFields = [
+        { label: 'Ref Number', value: refNumber },
+        { label: 'Transaction Type', value: orderType ? orderType : "" },
+        { label: 'Price /gpuhr', value: price ? `$ ${price}` : "" },
+        { label: 'GPU Count', value: gpuCount ? gpuCount : "" },
+        { label: 'Scheduled for', value: time ? formatDateTime(new Date(time)) : "" },
+        { label: 'Duration', value: duration ? `${duration} hours` : "" },
+        { label: 'Total', value: total ? `$ ${total.toLocaleString()}` : "" }
+    ].filter(field => field.value);
+
+    const animationVariants = {
+        initial: { opacity: 0.5, y: 10 },
+        animate: { opacity: 1, y: 5 },
+    };
+
+    return (
+        <div className="font-mono">
+            <div className="text-sm bold mt-4 pl-2 mb-1">
+                Order Summary
+            </div>
+            <div className="px-2 py-2 border-t border-gray-200">
+                {receiptFields.map((field, index) => (
+                    <div key={index} className="w-full font-mono text-xs flex justify-between py-1">
+                        <div className="text-left text-gray-500">
+                            {field.label}
+                        </div>
+                        <AnimatePresence>
+                            <div className="text-right">
+                                {field.value
+                                    ? field.value.split('').map((char, charIndex) => (
+                                        <motion.span
+                                            key={`${field.label}-${charIndex}`}
+                                            initial={{ opacity: 0, y: 20 }}
+                                            animate={{ opacity: 1, y: 0 }}
+                                            transition={{ duration: 0.15 }}
+                                            exit={{
+                                                opacity: 0,
+                                                y: 100,
+                                                transition: {
+                                                    duration: 0.15,
+                                                },
+                                            }}
+                                            className={`inline-block ${char === ' ' ? 'mr-1' : ' '} ${field.label === "Total" ? 'font-bold' : ' '}`}
+                                        >
+                                            {char}
+                                        </motion.span>
+                                    ))
+                                    : '-'
+                                }
+                            </div>
+                        </AnimatePresence>
+                    </div>
+                ))}
+            </div>
+        </div>
+
+    );
+};
+
+import { v4 as uuidv4 } from 'uuid';
+
 const PlaceOrderForm: React.FC = () => {
+    const [orderType, setOrderType] = useState<string>(''); // market buy, limit buy, market sell, limit sell
+    const [price, setPrice] = useState<string>('');
+    const [gpus, setGpus] = useState<string>('');
+    const marketPrice = "4.31" // let's say it's this for now
+    const defaultGpuCount = "1"
+    const [isFilled, setIsFilled] = useState(false); // coloring the dollar sign
+    const [refNumber, _] = useState(uuidv4());
+
+
+    // date stuff
+    const initialDate = new Date();
+    initialDate.setHours(initialDate.getHours() + 1, 0, 0, 0); // Round to the next hour
+    const [startDate, setStartDate] = useState<Date>(initialDate);
+    const [duration, setDuration] = useState(25); // Default value
+
+    const total = parseFloat(price) * parseInt(gpus, 10);
+    const receiptData = {
+        startDate,
+        duration,
+        refNumber: refNumber, // random for now
+        orderType, // taken from RadioGroup
+        price, // taken from CustomForm
+        gpuCount: gpus, // taken from CustomForm
+        time: startDate.toLocaleString(), // taken from the CustomForm's DateTimeSlider Component
+        total: isNaN(total) ? 0 : total, // Calculate total
+    };
+
     return (
         <>
             <Spacer y={3} />
-            <RadioGroup orientation='horizontal' className="max-w-xl w-full text-xxs">
-                <CustomRadio value="buy">
+            <RadioGroup
+                orientation='horizontal'
+                className="max-w-xl w-full text-xxs">
+                <CustomRadio value="Buy" onChange={() => setOrderType("Buy")}>
                     Buy
                 </CustomRadio>
-                <CustomRadio description="" value="sell">
+                <CustomRadio description="" value="Sell" onChange={() => setOrderType("Sell")}>
                     Sell
                 </CustomRadio>
-                <Spacer y={3} />
+                <Spacer y={4} />
             </RadioGroup>
             <Spacer y={1.5} />
-            <CustomForm />
+            <CustomForm
+                price={price}
+                setPrice={setPrice}
+                gpus={gpus}
+                setGpus={setGpus}
+                setStartDate={setStartDate}
+                duration={duration}
+                setDuration={setDuration}
+                marketPrice={marketPrice}
+                defaultGpuCount={defaultGpuCount}
+                setIsFilled={setIsFilled}
+                isFilled={isFilled}
+            />
+            <Receipt receiptData={receiptData} />
+            {/* <Submit /> */}
         </>
     );
 };
